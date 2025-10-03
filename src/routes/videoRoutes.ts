0dbeec248329bb6c64e57fa8b549fa7701905b1e
AnuOluwatomiwa <bamideleisrael04@gmail.com>
2025-10-03 23:38:40 +0100

    chore: wire video metadata controller into videoRoutes

--- src/routes/videoRoutes.ts
+++ src/routes/videoRoutes.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
-import { getVideoMetadata } from "../controllers/videoController.ts";
+import { getVideoMetadata, addVideo, getVideos, updateVideo, deleteVideo } from "../controllers/videoController.ts";
+import { authMiddleware } from "../middlewares/authMiddleware.ts";
 
 const router = Router();
 
 router.post("/metadata", getVideoMetadata);
+
+// Protected CRUD endpoints
+router.post("/", authMiddleware, addVideo);
+router.get("/", authMiddleware, getVideos);
+router.put("/:id", authMiddleware, updateVideo);
+router.delete("/:id", authMiddleware, deleteVideo);
 
 export default router;
